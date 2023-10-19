b9269e548100e5bdc5c12c31c1f873df3d7de692
Definitely-Human <odest66@gmail.com>
2023-10-19 16:39:38 +0200

    Fixed tests.

--- src/lists/lists.service.spec.ts
+++ src/lists/lists.service.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CategoriesService } from '../categories/categories.service';
+import { ListRepository } from './lists.repository';
 import { ListsService } from './lists.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  findOneWithCharacter: jest.fn(),
+  find: jest.fn(),
+  findWithCharacter: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+  delete: jest.fn(),
+};
+
+const mockCategoriesService = () => ({
+  getCategory: jest.fn(),
+});
 
 describe('ListsService', () => {
   let service: ListsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ListsService],
+      providers: [
+        ListsService,
+        { provide: ListRepository, useValue: mockRepository },
+        {
+          provide: CategoriesService,
+          useValue: mockCategoriesService(),
+        },
+      ],
     }).compile();
 
     service = module.get<ListsService>(ListsService);
